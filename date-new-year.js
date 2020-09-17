f0b7257a16393bcd960bbf4c54e98432ac038792
Natia Nakashidze <n.nakashidze09@gmail.com>
2020-09-17 04:20:52 +0400

    axal wlamde dro intervalit

--- date-new-year.js
+++ date-new-year.js
@@ -1,25 +1,13 @@
-// Date.now();
-// Date.UTC()
-// // Date.prototype.getDate()
-// Date.prototype.getTime()
-// console.log (Date.prototype.getTime());
-
-// const utcDate1 = new Date(Date.UTC(96, 1, 2, 3, 4, 5));
-// const utcDate2 = new Date(Date.UTC(0, 0, 0, 0, 0, 0));
-// // console.log(utcDate1.toUTCString());
-// console.log(utcDate2.toUTCString());
-// Date.UTC(year[, month[, day[, hour[, minute[, second[, millisecond]]]]]])
-// let utcDate1=Date.UTC(1970, 09, 16, 11, 12, 7);
-// console.log(utcDate1);
-
-let x=new Date("2021/1/1").getTime();
-let y=new Date().getTime();
+let x=new Date("2021/1/1").getTime(); //dro 1970 dan 01.01.2021mde
+let y=new Date().getTime(); // dro 1970 dan dgemde
 let milliSeconds=x-y;
-let milliSeconds2=milliSeconds%1000;
+setInterval((a) => {
+    return a=a-1000;
+}, 1000, milliSeconds);
 let seconds=Math.floor(milliSeconds/1000%60);
 let minute=Math.floor(milliSeconds/1000/60%60);
 let hours=Math.floor(milliSeconds/1000/60/60%60);
-let day=Math.floor(milliSeconds/1000/60/60/24%365);
-let year=Math.floor(milliSeconds/1000/60/60/24/365);
-console.log(year, day, hours, minute, seconds, milliSeconds2);
+let day=Math.floor(milliSeconds/1000/60/60/24);
+console.log(day, hours, minute, seconds);
+
 
