6848ce6a7c558de2cbf712b3ce7bf5625cbe18a4
Natia Nakashidze <n.nakashidze09@gmail.com>
2020-09-17 16:25:03 +0400

    drois gamotvla dasrulebuli

--- date-new-year.js
+++ date-new-year.js
@@ -1,13 +1,17 @@
 let x=new Date("2021/1/1").getTime(); //dro 1970 dan 01.01.2021mde
 let y=new Date().getTime(); // dro 1970 dan dgemde
 let milliSeconds=x-y;
-setInterval((a) => {
-    return a=a-1000;
-}, 1000, milliSeconds);
-let seconds=Math.floor(milliSeconds/1000%60);
-let minute=Math.floor(milliSeconds/1000/60%60);
-let hours=Math.floor(milliSeconds/1000/60/60%60);
-let day=Math.floor(milliSeconds/1000/60/60/24);
-console.log(day, hours, minute, seconds);
+setInterval(printTime, 1000);
+
+function printTime(){
+    console.clear();
+    milliSeconds-=1000;
+    let seconds=Math.floor(milliSeconds/1000%60);
+    let minute=Math.floor(milliSeconds/1000/60%60);
+    let hours=Math.floor(milliSeconds/1000/60/60%60);
+    let day=Math.floor(milliSeconds/1000/60/60/24);
+    console.log(day + " Days,", hours + " hours,", minute + " minute,", seconds + " seconds,");
+}
+
 
 
