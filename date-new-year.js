568b30f33350dce85f90931cc17590e1920f9dfa
Natia Nakashidze <n.nakashidze09@gmail.com>
2020-09-17 18:18:30 +0400

    dasrulebuli. daemata gacherebis funqcia

--- date-new-year.js
+++ date-new-year.js
@@ -1,17 +1,19 @@
-let x=new Date("2021/1/1").getTime(); //dro 1970 dan 01.01.2021mde
+let x=new Date("2021/1/1").getTime(); //dro 1970 dan 01.01.2021 mde
 let y=new Date().getTime(); // dro 1970 dan dgemde
 let milliSeconds=x-y;
-setInterval(printTime, 1000);
+let myInterval = setInterval(printTime, 1000);
 
 function printTime(){
     console.clear();
     milliSeconds-=1000;
+    if (milliSeconds <= 0){
+        clearInterval(myInterval);
+        console.log("Happy new year!!!");
+        return;
+    }
     let seconds=Math.floor(milliSeconds/1000%60);
     let minute=Math.floor(milliSeconds/1000/60%60);
     let hours=Math.floor(milliSeconds/1000/60/60%60);
     let day=Math.floor(milliSeconds/1000/60/60/24);
     console.log(day + " Days,", hours + " hours,", minute + " minute,", seconds + " seconds,");
 }
-
-
-
